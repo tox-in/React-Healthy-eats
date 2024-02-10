cd59666d6f9ed97d7cc3c83d0d0bc5013a2acbe1
tox-in <habimanat78@gmail.com>
2024-02-11 01:10:14 +0200

    Initial files

---
+++ src/data/data.js
@@ -0,0 +1,310 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Double Cheeseburger',
+      category: 'burger',
+      image:
+        'https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
+      price: '$$$$',
+    },
+    {
+      id: 2,
+      name: 'Bacon Cheeseburger',
+      category: 'burger',
+      image:
+        'https://images.unsplash.com/photo-1553979459-d2229ba7433b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
+      price: '$',
+    },
+    {
+      id: 3,
+      name: 'Mushroom Burger',
+      category: 'burger',
+      image:
+        'https://images.unsplash.com/photo-1608767221051-2b9d18f35a2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
+      price: '$$',
+    },
+    {
+      id: 4,
+      name: 'Loaded Burger',
+      category: 'burger',
+      image:
+        'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
+      price: '$$$',
+    },
+    {
+      id: 5,
+      name: 'Feta & Spinnach',
+      category: 'pizza',
+      image:
+        'https://images.unsplash.com/photo-1593560708920-61dd98c46a4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+      price: '$$',
+    },
+    {
+      id: 6,
+      name: 'Supreme Pizza',
+      category: 'pizza',
+      image:
+        'https://images.unsplash.com/photo-1604382355076-af4b0eb60143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+      price: '$$$',
+    },
+    {
+      id: 7,
+      name: 'Meat Lovers',
+      category: 'pizza',
+      image:
+        'https://images.unsplash.com/photo-1628840042765-356cda07504e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+      price: '$$$$',
+    },
+    {
+      id: 8,
+      name: 'Cheese Pizza',
+      category: 'pizza',
+      image:
+        'https://images.unsplash.com/photo-1548369937-47519962c11a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2hlZXNlJTIwcGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+      price: '$',
+    },
+    {
+      id: 9,
+      name: 'Kale Salad',
+      category: 'salad',
+      image:
+        'https://images.unsplash.com/photo-1515543237350-b3eea1ec8082?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+      price: '$$',
+    },
+    {
+      id: 10,
+      name: 'Ceasar Salad',
+      category: 'salad',
+      image:
+        'https://images.unsplash.com/photo-1546793665-c74683f339c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+      price: '$$$',
+    },
+    {
+      id: 11,
+      name: 'Loaded Salad',
+      category: 'salad',
+      image:
+        'https://images.unsplash.com/photo-1540420773420-3366772f4999?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FsYWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+      price: '$$$$',
+    },
+    {
+      id: 12,
+      name: 'Fruit Salad',
+      category: 'salad',
+      image:
+        'https://images.unsplash.com/photo-1564093497595-593b96d80180?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJ1aXQlMjBzYWxhZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+      price: '$',
+    },
+    {
+      id: 13,
+      name: 'Wings',
+      category: 'chicken',
+      image:
+        'https://images.unsplash.com/photo-1567620832903-9fc6debc209f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+      price: '$$',
+    },
+    {
+      id: 14,
+      name: 'Baked Chicken',
+      category: 'chicken',
+      image:
+        'https://images.unsplash.com/photo-1594221708779-94832f4320d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+      price: '$$$$',
+    },
+    {
+      id: 15,
+      name: 'Chicken Tenders',
+      category: 'chicken',
+      image:
+        'https://images.unsplash.com/photo-1562967914-608f82629710?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+      price: '$',
+    },
+    {
+      id: 16,
+      name: 'Chicken Kabob',
+      category: 'chicken',
+      image:
+        'https://images.unsplash.com/photo-1603360946369-dc9bb6258143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+      price: '$$$',
+    },
+    {
+        id: 17,
+        name: 'Baked Cod Recipe with Lemon and Garlic',
+        category: 'fish',
+        image: 
+          'https://www.themediterraneandish.com/wp-content/uploads/2017/10/Mediterranean-Baked-Cod-Recipe-Lemon-Garlic-The-Mediterranean-Dish-1.jpg',
+        price: '$$$'
+    },
+    {
+        id: 18,
+        name: 'Mediterranean-Style Baked White Fish',
+        category: 'fish',
+        image:
+          'https://media.istockphoto.com/id/517313488/photo/baked-dorado-on-plate-on-white-background-with-lemon-dill.jpg?s=1024x1024&w=is&k=20&c=0YXflo6J899KRMnPIPSPVOFlwIhO5_tNbg0UxcZkWLw=',
+          price: '$'
+    },
+    {
+        id: 19,
+        name: 'Baked Lemon Garlic Salmon',
+        category: 'fish',
+        image:
+          'https://www.themediterraneandish.com/wp-content/uploads/2019/12/Lemon-Garlic-Salmon-Recipe-4.jpg',
+        price: '$$$$'
+    },
+    {
+        id: 20,
+        name: 'One-pan Mediterranean Baked Halibut Recipe with Vegetables',
+        category: 'fish',
+        image:
+          'https://www.themediterraneandish.com/wp-content/uploads/2016/05/One-Pan-Baked-Halibut-Recipe-7.jpg',
+        price: '$$'
+    },
+    {
+        id: 21,
+        name: 'Creamy Chicken Piccata With Gnocchi',
+        category: 'pasta',
+        image:
+          'https://hips.hearstapps.com/hmg-prod/images/creamy-chicken-piccata-with-gnocchi-secondary-643d867149e9a.jpg?crop=0.6666666666666666xw:1xh;center,top&resize=980:*',
+        price: '$$'
+    },
+    {
+        id: 22,
+        name: 'Chicken Alfredo',
+        category: 'pasta',
+        image:
+          'https://hips.hearstapps.com/hmg-prod/images/delish-221130-perfect-chicken-alfredo-0689-eb-1670449996.jpg?crop=0.774xw:0.856xh;0.115xw,0.0741xh&resize=980:*',
+        price: '$$$'
+    },
+    {
+        id: 23,
+        name: 'Classic Lasagna',
+        category: 'pasta',
+        image:
+          'https://media.istockphoto.com/id/1994451368/photo/italian-lasagna-with-tomato-sauce-isolated-on-white-background.jpg?s=1024x1024&w=is&k=20&c=NdyKH6SNnpzrThfdX3PulMdwtW-CZqqf_rNc6aLtUAw=',
+        price: '$$$$'
+    },
+    {
+        id: 24,
+        name: 'Roasted Red Pepper Shrimp Linguine',
+        category: 'pasta',
+        image:
+          'https://hips.hearstapps.com/hmg-prod/images/roasted-red-pepper-shrimp-linguine1-1661361008.jpg?crop=0.448xw:1.00xh;0.142xw,0&resize=980:*',
+          price: '$$$'
+    },
+    {
+        id: 25,
+        name: 'Pot-roast beef with French onion gravy',
+        category: 'roasted beef',
+        image:
+          'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1222463_11-7eaf5a2.jpg?quality=90&webp=true&resize=440,400',
+        price: '$$$'
+    },
+    {
+        id: 26,
+        name: 'Slow-roasted rib of beef',
+        category: 'roasted beef',
+        image:
+          'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/herb_scented_rib_beef-c866154.jpg?quality=90&webp=true&resize=440,400',
+        price: '$$$$'
+    },
+    {
+        id: 27,
+        name: 'Roast fillet of beef with shallot & mushroom sauce',
+        category: 'roasted beef',
+        image:
+          'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-16064_12-9684900.jpg?quality=90&webp=true&resize=440,400',
+        price: '$$'
+    },
+    {
+        id: 28,
+        name: 'Slow cooker beef topside with red wine gravy',
+        category: 'roasted beef',
+        image:
+          'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/slow-cooker-beef-topside-with-red-wine-gravy-e2408a2.jpg?quality=90&webp=true&resize=440,400',
+        price: '$$'
+    },
+    {
+        id: 29,
+        name: 'Potato Salad',
+        category: "others",
+        image: 
+          'https://img.taste.com.au/jte9RL0O/taste/2010/08/classic-potato-salad-118978-2.jpg',
+        price: '$$$'
+    },
+    {
+        id: 30,
+        name: 'Coleslaw',
+        category: 'others',
+        image:
+          'https://www.tasteofhome.com/wp-content/uploads/2022/07/Kentucky-Coleslaw_EXPS_RC22_267782_DR_04_21_2b.jpg',
+        price: '$$'
+    },
+    {
+        id: 31,
+        name: 'Baked Beans',
+        category: 'others',
+        image:
+          'https://www.howsweeteats.com/wp-content/uploads/2020/05/baked-beans-15.jpg',
+          price: '$$$'
+    },
+    {
+        id: 32,
+        name: 'Cornbread',
+        category: 'others',
+        image:
+          'https://www.recipetineats.com/wp-content/uploads/2019/11/Corn-bread_1-SQ.jpg',
+        price: '$$$'
+    }
+  ];
+  
+  export const categories = [
+    {
+      id: 1,
+      name: 'Fast Food',
+      image:
+        'https://images.unsplash.com/photo-1512152272829-e3139592d56f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 2,
+      name: 'Pizza',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Pizza_BrowseHome@3x.png',
+    },
+    {
+      id: 3,
+      name: 'Wings',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Wings_BrowseHome@3x.png',
+    },
+    {
+      id: 4,
+      name: 'Indian',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Indian_BrowseHome@3x.png',
+    },
+    {
+      id: 5,
+      name: 'Latest Deals',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Deals_BrowseHome@3x.png',
+    },
+    {
+      id: 6,
+      name: 'Restaurant Rewards',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/RestoRewards_BrowseHome@3x.png',
+    },
+    {
+      id: 7,
+      name: 'Best Overall',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/TopEats_Browse%20Home@3x.png',
+    },
+    {
+      id: 8,
+      name: 'Shipped Free',
+      image:
+        'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Placeholder_Plates@3x.png',
+    }
+  ];
+  
